7adee3e4eae9973b935ad49686609bf5c25d6ec9
Moussa Syoufi <mousasya3@gmail.com>
2023-01-11 20:09:54 +0300

    added aplusb problem

---
+++ Sample/aplusb.test.cpp
@@ -0,0 +1,99 @@
+/*
+           _                   _         _       __  __  _____ 
+     /\   | |            /\   | |       | |     |  \/  |/ ____|
+    /  \  | |__   ___   /  \  | |__   __| | ___ | \  / | |     
+   / /\ \ | '_ \ / _ \ / /\ \ | '_ \ / _` |/ _ \| |\/| | |     
+  / ____ \| |_) | (_) / ____ \| |_) | (_| | (_) | |  | | |____ 
+ /_/    \_\_.__/ \___/_/    \_\_.__/ \__,_|\___/|_|  |_|\_____|
+*/
+
+// build command:
+// g++ -std=gnu++17 -O3 -DDEBUG -g -fsanitize=signed-integer-overflow -fsanitize=bounds-strict -fsanitize=address -fsanitize=integer-divide-by-zero -fsanitize=float-divide-by-zero -fsanitize=pointer-overflow -fsanitize=shift-exponent -fsplit-stack -Wshadow -Wall -fconcepts -Wno-unused-result
+
+// REMEMBER:
+// BS, Offline, Persistent SegTree, SQRT, Treap, MaxFlow, FFT, Matrices
+
+#define PROBLEM "https://judge.yosupo.jp/problem/aplusb"
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+
+// DEBUG STUFF
+#define coutc "\033[48;5;196m\033[38;5;15m"
+#define endc "\033[0m"
+#define len(x) ((int)(x).size())
+#ifdef DEBUG
+string to_string(char c) { return string({c}); }
+
+template<class... Ts>
+ostream& operator<<(ostream& o, tuple<Ts...> t) {
+  string s = "(";
+  apply([&](auto&&... r) {
+    ((s += to_string(r) + ", "), ...); }, t);
+  return o << s.substr(0, len(s) - 2) + ")";
+}
+
+ostream& operator<<(ostream &o, pair<auto, auto> p) {
+  return o << "(" << p.fi << ", " << p.se << ")";
+}
+
+template<class C, class T = typename C::value_type,
+typename std::enable_if<!std::is_same<C, std::string>
+::value>::type* = nullptr>
+ostream& operator<<(ostream &o, C c) {
+  for (auto e : c) o << setw(7) << right << e;
+  return o << endc << endl << coutc;
+}
+
+void debug(const auto &e, const auto &... r) {
+  cerr << coutc << e;
+  ((cerr << " " << r), ..., (cerr << endc << endl));
+}
+#else
+#define debug(...)
+#endif
+
+
+#define F first
+#define S second
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((int)(x).size())
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+
+// RANDOM NUMBER GENERATOR
+mt19937 RNG(chrono::steady_clock::now().time_since_epoch().count());  
+#define SHUF(v) shuffle(all(v), RNG); 
+// Use mt19937_64 for 64 bit random numbers.
+
+int getrand(int l,int r) {
+	return uniform_int_distribution<int>(l, r)(RNG);
+}
+
+const ld eps = 1e-9;
+const int mod = 1e9 + 7;
+const int oo = 1e9 + 7;
+const ll lloo = 1e18 + 7;
+const int N = 1e6 + 7;
+
+void solve(int tc) {
+	int a,b;
+	scanf("%d %d",&a,&b);
+	printf("%d\n",a+b);
+}
+
+int main() {
+	// freopen("in","r",stdin);
+	// freopen("out","w",stdout);
+	int T = 1;
+	//scanf("%d",&T);
+	for(int i = 0 ; i < T ; i++) solve(i+1);
+	return 0;
+}
